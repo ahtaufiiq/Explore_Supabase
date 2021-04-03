2314f696669353d3eb9adc93e110a4d9d4d6e3c5
Ahmad Taufiq <ataufiq665@gmail.com>
2021-04-03 09:35:16 +0700

    crud supabase

---
+++ index.js
@@ -0,0 +1,56 @@
+require('dotenv').config();
+const {createClient} = require('@supabase/supabase-js')
+const supabaseUrl = process.env.SUPABASE_URL
+const supabaseKey = process.env.SUPABASE_KEY
+const supabase = createClient(supabaseUrl, supabaseKey)
+
+async function read() {
+    let { data: Todo, error } = await supabase
+        .from('Todo')
+        .select('*')
+        console.log(Todo);
+}
+async function update(name) {
+    const { data, error } = await supabase
+        .from('Todo')
+        .update({ name })
+        .eq('name', 'Testing')
+    
+}
+
+async function add(name) {
+    const { data, error } = await supabase
+        .from('Todo')
+        .insert([
+            { name },
+        ])
+}
+
+async function listen() {
+    const Todo = supabase
+        .from('Todo')
+        .on('*', payload => {
+            switch (payload.eventType) {
+                case "DELETE":
+                    console.log(`Delete ${payload.old.id}`);
+                    break;
+                case "INSERT":
+                    console.log(`Insert ${payload.new.name}`);
+                    break;
+                case "UPDATE":
+                    console.log(`Update ${payload.new.name}`);
+                    break;
+            
+                default:
+                    break;
+            }
+        })
+        .subscribe()
+        
+}
+
+
+listen()
+update("Belajar")
+add("Testing")
+read()
